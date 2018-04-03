64123dc59d06c068e4816764343b8b36c56e4ebf
Bruno Frank <bfscordeiro@gmail.com>
2018-04-03 20:44:07 -0300

    Fix dependency version

--- lib/trustvox/base.rb
+++ lib/trustvox/base.rb
@@ -5,8 +5,8 @@ module Trustvox
   class Base
     include HTTParty
 
-    headers 'Accept': 'application/vnd.trustvox.com; version=1',
-            'Content-Type': 'application/json'
+    headers 'Accept' => 'application/vnd.trustvox.com; version=1',
+            'Content-Type' => 'application/json'
 
     def initialize
       if Config.is_on_staging?
@@ -18,11 +18,11 @@ module Trustvox
     end
 
     def auth_by_platform_token!
-      self.class.headers 'Authorization': "token #{Config.token}"
+      self.class.headers 'Authorization' => "token #{Config.token}"
     end
 
     def auth_by_store_token!
-      self.class.headers 'Authorization': "token #{Config.store_token}"
+      self.class.headers 'Authorization' => "token #{Config.store_token}"
     end
   end
 end
